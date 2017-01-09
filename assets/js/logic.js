f73016725ad258a7a6be600a3213c2421938576b
Wesley Handy <wesleylhandy@gmail.com>
2017-01-09 12:25:49 -0500

    completed images can be saved to localstorage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,9 @@
+localStorage.clear();
 var degree = 359; //initialize degrees)
 var color = "rgba(0,0,255,0.90)"; // initial color
+var counter = 0; //used to store data in a numbered array
+var canvasArray = []; //used to push data to local storage for completed pic
+var spinning = false; //control for paint and storing functions
 
 $(document).ready(function(){
 
@@ -12,12 +16,17 @@ $(document).ready(function(){
 
 	/* 
 
-	1. create function to slow spin as it returns to initial position on stop 
+	DONE  1. create function to slow spin as it returns to initial position on stop 
 	2. create speed slider to speed up or slow down rotation
-	3. store paint drops into an array and push to local storage
-		with unique keys for each saved object, display mini version
-		of the saved image in a right-hand side-bar
-	4. style the background and controls
+	DONE 3. store paint drops into an array and push to local storage
+		with unique keys for each saved object, 
+	4. display mini version of the saved image in a right-hand side-bar
+	5. limit right hand display to the previous 4 obj
+	6. allow users to click on mini-whirls and redisplay on main canvas
+	7. allow users to delete a whirl
+	8. allow users to pring to pdf
+	9. allow users to tweet image of whirl
+	10. style the background and controls
 
 	*/
 
@@ -105,24 +114,83 @@ $(document).ready(function(){
 			y: event.clientY - window.innerHeight / 2
 		}
 
-		//convert points relative to the degree of spin on the canvas
-		points = convertPoint(points.x, points.y); 
+
 
 		// console.log("Mousepos at X: " + points.x + " & Y: " + points.y);
 
 		if (spinning) {
+
+			//convert points relative to the degree of spin on the canvas
+			//will return converted points, plus coordinates for teardrop, as object
+			points = convertPoint(points.x, points.y); 
+
 			//painting on canvas only if canvas is in spin mode
 			$("#canvas-wrapper").css("background-image", 'radial-gradient(#000 0%, #555 55%, ' + color + ' 70%)');
-			dripPaint(points.x, points.y);	
+			dripPaint(points);	
 		}
 	});
 
 	function convertPoint(x,y){
 
-	    x2 = x * Math.cos(radians(degree)) - y * Math.sin(radians(degree));
-	    y2 = x * Math.sin(radians(degree)) + y * Math.cos(radians(degree));
+		//convert static points to translated points on a rotated canvas
+		//note: canvas x,y is inverted 
+	    xPrime = x * Math.cos(radians(degree)) - y * Math.sin(radians(degree));
+	    yPrime = x * Math.sin(radians(degree)) + y * Math.cos(radians(degree));
 
-	    return {x: x2, y: y2};
+	    //formulas for calculating a point a set distance along a line
+		//given x, y, distance, and slope
+			//x1 = x +- sqrt(distance^2/(1 + slope^2))
+			//y1 = slope * (x1 - x) + y;
+
+		//slopes
+		var m = yPrime/xPrime; //slope = change in rise / change in run
+		var m1 = -(xPrime/yPrime); //negative inverse, slope of line perpendicular to m
+		// console.log("slopes = " + m + ", " + m1);
+
+		//distances
+		var d0 = 75; //distance along line
+		var d1 = 12; // control point for curve
+		var d2 = 7.5; //distance along perpendicular
+
+		// console.log("distances = " + d + ', ' + d1 + ', ' + d2);
+
+		//calculate coordinates of a point along a line on a slope
+
+		//check x position to determine which direction to move along line
+		if (xPrime > 0) {
+			var x0 = xPrime - Math.sqrt((d1*d1)/(1 + (m*m))); //control point 
+			var x1 = xPrime + Math.sqrt((d0*d0)/(1 + (m*m))); //tip
+			var y0 = m * (x0 - xPrime) + yPrime; //control point
+			var y1 = m * (x1 - xPrime) + yPrime;	//tip
+		} else {
+			var x0 = xPrime + Math.sqrt((d1*d1)/(1 + (m*m))); //control point 
+			var x1 = xPrime - Math.sqrt((d0*d0)/(1 + (m*m))); //tip
+			var y0 = m * (x0 - xPrime) + yPrime; //control point
+			var y1 = m * (x1 - xPrime) + yPrime; //tip
+		}
+		// console.log("point1= " + x1 + ', ' + y1);
+
+		//calculate coordinate of a point along a line perpendicular to x1, y1
+		var x2 = xPrime - Math.sqrt((d2 * d2)/(1 + (m1*m1)));
+		var y2 = m1 * (x2 - xPrime) + yPrime;
+
+		// console.log("point2= " + x2 + ', ' + y2);
+
+		//calculate coordinate of a point in other direction from x2, y2
+		var x3 = xPrime + Math.sqrt((d2 * d2)/(1 + (m1*m1)));
+		var y3 = yPrime + m1 * (x3 - xPrime);
+		// console.log("point3= " + x3 + ', ' + y3); 
+
+		//store coordinates in local storage
+		var drop = {coords: {m: m, m1: m1, 
+								d0: d0, d1: d1, d2: d2, 
+								x: xPrime, x0: x0, x1: x1, x2: x2, x3: x3, 
+								y: yPrime, y0: y0, y1: y1, y2: y2, y3: y3, 
+								color: color}};
+		console.log(drop);
+		canvasArray.push(drop);
+
+	    return drop;
 	}
 
 	function radians(degrees){
@@ -133,71 +201,33 @@ $(document).ready(function(){
 	//the end goal is for there to be a teardrop of paint moving
 	//pointing in direction of the current angle
 
-	function dripPaint(x, y) {
+	function dripPaint(obj) {
 
-		//formulas for calculating a point a set distance along a line
-		//given x, y, distance, and slope
-			//x1 = x +- sqrt(distance^2/(1 + slope^2))
-			//y1 = slope * (x1 - x) + y;
-
-		//slopes
-		var m = y/x; //slope = change in rise / change in run
-		var m1 = -(x/y); //negative inverse, slope of line perpendicular to m
-		// console.log("slopes = " + m + ", " + m1);
-
-		//distances
-		var d = 75; //distance along line
-		var d1 = 12; // control point for curve
-		var d2 = 7.5; //distance along perpendicular
-
-		// console.log("distances = " + d + ', ' + d1 + ', ' + d2);
-
-		//calculate coordinates of a point along a line on a slope
-
-		//check x position to determine which direction to move along line
-		if (x > 0) {
-			var x0 = x - Math.sqrt((d1*d1)/(1 + (m*m))); //control point 
-			var x1 = x + Math.sqrt((d*d)/(1 + (m*m))); //tip
-			var y0 = m * (x0 - x) + y; //control point
-			var y1 = m * (x1 - x) + y;	//tip
-		} else {
-			var x0 = x + Math.sqrt((d1*d1)/(1 + (m*m))); //control point 
-			var x1 = x - Math.sqrt((d*d)/(1 + (m*m))); //tip
-			var y0 = m * (x0 - x) + y; //control point
-			var y1 = m * (x1 - x) + y; //tip
-		}
-		// console.log("point1= " + x1 + ', ' + y1);
-
-		//calculate coordinate of a point along a line perpendicular to x1, y1
-		var x2 = x - Math.sqrt((d2 * d2)/(1 + (m1*m1)));
-		var y2 = m1 * (x2 - x) + y;
-
-		// console.log("point2= " + x2 + ', ' + y2);
-
-		//calculate coordinate of a point in other direction from x2, y2
-		var x3 = x + Math.sqrt((d2 * d2)/(1 + (m1*m1)));
-		var y3 = y + m1 * (x3 - x);
-		// console.log("point3= " + x3 + ', ' + y3); 
+		console.log(obj);
 
 		ctx.beginPath();
 
 		//draw small circle at tip of tear
-		ctx.moveTo(x1, y1);
-		ctx.arc(x1, y1, d/80, 0, Math.PI *2);
-		ctx.moveTo(x1, y1);
+		ctx.moveTo(obj.coords.x1, obj.coords.y1);
+		ctx.arc(obj.coords.x1, obj.coords.y1, obj.coords.d0/80, 0, Math.PI *2);
+		ctx.moveTo(obj.coords.x1, obj.coords.y1);
 		
 		//draw triangle towards original point
-		ctx.lineTo(x2,y2);//side 1 of triangle
+		ctx.lineTo(obj.coords.x2,obj.coords.y2);//side 1 of triangle
 
-		ctx.quadraticCurveTo(x0, y0, x3, y3); //curved side of triangle
+		ctx.quadraticCurveTo(obj.coords.x0, obj.coords.y0, obj.coords.x3, obj.coords.y3); //curved side of triangle
 
-		ctx.lineTo(x1, y1);//side 3 of triangle
+		ctx.lineTo(obj.coords.x1, obj.coords.y1);//side 3 of triangle
 		
-		ctx.fillStyle = color; //future function to change color
-		ctx.strokeStyle = color;
+		ctx.fillStyle = obj.coords.color; //future function to change color
+		ctx.strokeStyle = obj.coords.color;
 		ctx.lineWidth = 6;
 		ctx.fill();
 		ctx.stroke();
+	}
+
+	function drawMiniWhirl (obj) {
+
 	}
 
 	//get canvas and context
@@ -205,7 +235,7 @@ $(document).ready(function(){
 	var ctx = canvas.getContext("2d");
 	var width = parseInt($("#canvas").attr("width"));
 	var height = parseInt($("#canvas").attr("height"));
-	var spinning = false;
+
 
 	//move ctx to center
 	ctx.translate(width/2, height/2);
@@ -240,7 +270,14 @@ $(document).ready(function(){
 		spinToZero();
 	});
 
+	$("#save-btn").on("click", function(){
+		localStorage.setItem("canvas" + counter, JSON.stringify(canvasArray));
+		console.log(localStorage);
+		counter++;
+	});
+
 	$("#clear-btn").on("click", function(){
+		canvasArray = [];
 		canvas.width = canvas.width;
 		ctx = canvas.getContext("2d");
 		ctx.translate(width/2, height/2);
@@ -278,7 +315,7 @@ $(document).ready(function(){
 	});
 
 	$("#violet").on("click", function(){
-		color="rgba(238,130,238,0.90)";
+		color="rgba(238,130,238,0.90)"	;
 		console.log("color: " + color);
 	});
 
