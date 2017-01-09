ac682434f160853122d2d1a8a8e888c3d0b5737e
Wesley Handy <wesleylhandy@gmail.com>
2017-01-09 00:22:15 -0500

    added more colors and fixed rotation conversion

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,5 @@
 var degree = 359; //initialize degrees)
-var color = "blue"; // initial color
+var color = "rgba(0,0,255,0.90)"; // initial color
 
 $(document).ready(function(){
 
@@ -25,7 +25,7 @@ $(document).ready(function(){
 
 		//draw a circle at the middle
 
-			var radius = 5;
+			var radius = 7;
 			ctx.beginPath();
 			ctx.arc(0,0,radius,0,Math.PI*2);
 			ctx.fillStyle = "black";
@@ -52,8 +52,6 @@ $(document).ready(function(){
 	//Canvas spin function
 		//will spin canvas css to show user the spin
 
-
-
 	function spinCanvas() {
 		
 		$("#canvas").css("transform", "rotate(" + degree + "deg)");
@@ -61,6 +59,7 @@ $(document).ready(function(){
 		//will rotate canvas to spin in sequence with css rotate
 		ctx.rotate(radians(degree)); 
 		
+		//counter-clockwise rotation
 		degree--;
 
 		if (degree < 0) {
@@ -76,10 +75,11 @@ $(document).ready(function(){
 	function spinToZero () {
 		
 		if (degree === 0) {
-
+			//stop recursion, canvas will be in regular position
 			return false;
 		} else {
 
+			//every 20 degrees reduce the spin by increasing the timeout
 			if (degree % 20 === 0) {
 				spinTime++;
 			}
@@ -98,26 +98,29 @@ $(document).ready(function(){
 	//event listener for mousepressed over canvas
 	$("#canvas").mousemove(function(event){
 
-		
+		//get mouse position in relation to the browser window and 
+		//set them to a position relative to canvas
 		var points = {
 			x: event.clientX - window.innerWidth / 2 ,
 			y: event.clientY - window.innerHeight / 2
 		}
-		//points = convertPoint(points.x, points.y); 
-		console.log("Mousepos at X: " + points.x + " & Y: " + points.y);
+
+		//convert points relative to the degree of spin on the canvas
+		points = convertPoint(points.x, points.y); 
+
+		// console.log("Mousepos at X: " + points.x + " & Y: " + points.y);
+
 		if (spinning) {
-			$("#canvas-wrapper").css("background-image", 'radial-gradient(black 0%, grey 55%, ' + color + ' 70%)');
+			//painting on canvas only if canvas is in spin mode
+			$("#canvas-wrapper").css("background-image", 'radial-gradient(#000 0%, #555 55%, ' + color + ' 70%)');
 			dripPaint(points.x, points.y);	
 		}
 	});
 
 	function convertPoint(x,y){
 
-	    
-	    var offset = 0; //change if your corner is not 0,0
-
-	    x2 = x * Math.cos(radians(degree)) + y * Math.sin(radians(degree));
-	    y2 = x * Math.sin(radians(degree)) - y * Math.cos(radians(degree));
+	    x2 = x * Math.cos(radians(degree)) - y * Math.sin(radians(degree));
+	    y2 = x * Math.sin(radians(degree)) + y * Math.cos(radians(degree));
 
 	    return {x: x2, y: y2};
 	}
@@ -130,57 +133,28 @@ $(document).ready(function(){
 	//the end goal is for there to be a teardrop of paint moving
 	//pointing in direction of the current angle
 
-	
-
-	//user presses key to change color
-	$(document).on("keydown", function(event) {
-
-		//====================
-		//	THE W KEY
-		//====================
-		if (event.keyCode == 87) {
-			color = "blue";
-		}
-		//====================
-		//	THE S KEY
-		//====================
-		if (event.keyCode == 83) {
-			color = "orange";
-		}
-		//====================
-		//	THE A KEY
-		//====================
-		if (event.keyCode == 65) {
-			color = "green";
-		}
-		//====================
-		//	THE D KEY
-		//====================
-		if (event.keyCode == 68) {
-			color = "red";
-		}
-
-		console.log("color: " + color);
-	});
-
 	function dripPaint(x, y) {
 
+		//formulas for calculating a point a set distance along a line
+		//given x, y, distance, and slope
+			//x1 = x +- sqrt(distance^2/(1 + slope^2))
+			//y1 = slope * (x1 - x) + y;
+
+		//slopes
 		var m = y/x; //slope = change in rise / change in run
-
-
 		var m1 = -(x/y); //negative inverse, slope of line perpendicular to m
-		console.log("slopes = " + m + ", " + m1);
+		// console.log("slopes = " + m + ", " + m1);
 
-
-		//draw teardropish shape
+		//distances
 		var d = 75; //distance along line
 		var d1 = 12; // control point for curve
 		var d2 = 7.5; //distance along perpendicular
 
-
-		console.log("distances = " + d + ', ' + d1 + ', ' + d2);
+		// console.log("distances = " + d + ', ' + d1 + ', ' + d2);
 
 		//calculate coordinates of a point along a line on a slope
+
+		//check x position to determine which direction to move along line
 		if (x > 0) {
 			var x0 = x - Math.sqrt((d1*d1)/(1 + (m*m))); //control point 
 			var x1 = x + Math.sqrt((d*d)/(1 + (m*m))); //tip
@@ -192,26 +166,32 @@ $(document).ready(function(){
 			var y0 = m * (x0 - x) + y; //control point
 			var y1 = m * (x1 - x) + y; //tip
 		}
-		console.log("point1= " + x1 + ', ' + y1);
+		// console.log("point1= " + x1 + ', ' + y1);
+
 		//calculate coordinate of a point along a line perpendicular to x1, y1
 		var x2 = x - Math.sqrt((d2 * d2)/(1 + (m1*m1)));
 		var y2 = m1 * (x2 - x) + y;
 
-		console.log("point2= " + x2 + ', ' + y2);
+		// console.log("point2= " + x2 + ', ' + y2);
+
 		//calculate coordinate of a point in other direction from x2, y2
 		var x3 = x + Math.sqrt((d2 * d2)/(1 + (m1*m1)));
 		var y3 = y + m1 * (x3 - x);
-		console.log("point3= " + x3 + ', ' + y3); 
+		// console.log("point3= " + x3 + ', ' + y3); 
 
 		ctx.beginPath();
-		//draw drop at tip of tear
+
+		//draw small circle at tip of tear
 		ctx.moveTo(x1, y1);
 		ctx.arc(x1, y1, d/80, 0, Math.PI *2);
 		ctx.moveTo(x1, y1);
-		//draw triangle
-		ctx.lineTo(x2,y2);//corner 1 of triangle
-		ctx.quadraticCurveTo(x0, y0, x3, y3); //draw curved drop
-		ctx.lineTo(x1, y1);//final corner of triangle
+		
+		//draw triangle towards original point
+		ctx.lineTo(x2,y2);//side 1 of triangle
+
+		ctx.quadraticCurveTo(x0, y0, x3, y3); //curved side of triangle
+
+		ctx.lineTo(x1, y1);//side 3 of triangle
 		
 		ctx.fillStyle = color; //future function to change color
 		ctx.strokeStyle = color;
@@ -245,15 +225,16 @@ $(document).ready(function(){
 	var spin; //initialize interval variable
 
 	$("#spin-btn").on("click", function(){
-		if (!spinning) {
+		if (!spinning) { // prevent multiple intervals
 			spinning = true;
-			spin = setInterval(spinCanvas, 1);
+			spin = setInterval(spinCanvas, 1); //continuously spin
+			// spinCanvas(); // spin one degree
 		}
 	});
 
 	$("#stop-btn").on("click", function(){
-		spinning = false;
-		clearInterval(spin);
+		spinning = false; //prevent more painting
+		clearInterval(spin); //stop spin
 		console.log(degree);
 		//call slow return to zero function
 		spinToZero();
@@ -267,25 +248,38 @@ $(document).ready(function(){
 	});
 
 	$("#blue").on("click", function(){
-		color = "blue";
+		color = "rgba(0, 0, 255, 0.90)";
 		console.log("color: " + color);
 	});
 
 	$("#red").on("click", function(){
-		color="red";
+		color="rgba(255,0,0,0.90)";
 		console.log("color: " + color);
 	});
 
 	$("#green").on("click", function(){
-		color="green";
+		color="rgba(0,128,0,0.90)";
 		console.log("color: " + color);
 	});
 
 	$("#orange").on("click", function(){
-		color="orange";
+		color="rgba(255, 165, 0, 0.90)";
 		console.log("color: " + color);
 	});
 
-	
-});
+	$("#yellow").on("click", function(){
+		color="rgba(255, 255, 0, 0.90)";
+		console.log("color: " + color);
+	});
 
+	$("#indigo").on("click", function(){
+		color="rgba(75, 0, 130, 0.90)";
+		console.log("color: " + color);
+	});
+
+	$("#violet").on("click", function(){
+		color="rgba(238,130,238,0.90)";
+		console.log("color: " + color);
+	});
+
+});
